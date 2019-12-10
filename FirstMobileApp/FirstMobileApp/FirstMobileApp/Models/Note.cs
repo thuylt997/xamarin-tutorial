4f67bda3f0151bce8bd2bbad3e4155282eaf3450
thuylt <thuylt@runsystem.net>
2019-12-10 23:59:02 +0700

    Re-upload project

---
+++ FirstMobileApp/FirstMobileApp/FirstMobileApp/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿/**
+ * This class defines a Note model whose instances 
+ * store data about each note in the application.
+ */
+
+using System;
+using SQLite;
+
+namespace FirstMobileApp.Models
+{
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        //public string fileName { get; set; }
+        public int id { get; set; }
+        public string text { get; set; }
+        public DateTime date { get; set; }
+    }
+}
