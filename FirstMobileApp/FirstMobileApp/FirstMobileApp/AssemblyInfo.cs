fe29a7125da31b3a7e529187b8bca158cce17e3e
thuylt <thuylt@runsystem.net>
2019-12-10 23:58:06 +0700

    Added .gitattributes file

--- FirstMobileApp/FirstMobileApp/FirstMobileApp/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-/**
- * This file contains an application attribute about
- * the project, that is applied at the assembly level.
- */
-
-using Xamarin.Forms.Xaml;
-
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
